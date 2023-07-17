c4871f6b7b8f10f440748ccbaa4bee736e5c091a
vic-meateater <victor.myasoedov@gmail.com>
2023-07-17 06:48:49 +0500

    add proj

---
+++ Assets/_Root/Scripts/Authorization.cs
@@ -0,0 +1,109 @@
+using System;
+using Fusion;
+using Fusion.Photon.Realtime;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+    
+
+
+public class Authorization : MonoBehaviour
+{
+    private const string AUTH_KEY = "player-unique-id";
+    
+    //[SerializeField] private string _playFabTitle;
+    [SerializeField] private InputField _username;
+    [SerializeField] private InputField _password;
+    [SerializeField] private Button _signInButton;
+    
+    
+    //private readonly string _customId = "TestAuth";
+    private NetworkRunner _runner;
+
+    private void Start()
+    {
+        // if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+        //     PlayFabSettings.staticSettings.TitleId = _playFabTitle;
+        //
+        _signInButton.onClick.AddListener(Login);
+        if (PlayerPrefs.HasKey(AUTH_KEY))
+        {
+            Debug.Log("PlayerPrefs.HasKey");
+            var customId  = PlayerPrefs.GetString(AUTH_KEY, Guid.NewGuid().ToString());
+            var request = new LoginWithCustomIDRequest
+            {
+                CustomId = customId,
+                CreateAccount = false
+            };
+            PlayFabClientAPI.LoginWithCustomID(request,ResultCallback,
+            error => Debug.Log(error));
+        }
+        else
+        {
+            Debug.Log("Авторизируйтесь по паролю");
+        }
+
+
+        // Connect();
+    }
+
+    private void ResultCallback(LoginResult result)
+    {
+        Debug.Log(result.PlayFabId);
+        Debug.Log(result.AuthenticationContext);
+        Debug.Log(result.EntityToken);
+        Debug.Log(result.SessionTicket);
+        var token = result.EntityToken;
+        var ticket = result.SessionTicket;
+        
+        //_runner.StartGame(new StartGameArgs() { });
+        // Runner.Run(result);
+        // NetworkObject
+        // PhotonNetwork.AuthValues = new AuthenticationValues(result.PlayFabId);
+        // PhotonNetwork.NickName = _customId;
+        
+        // PlayFabClientAPI.LinkCustomID(LinkCustomIDRequest(), result =>
+        // {
+        //     Debug.Log("Link successfully");
+        // }, error =>
+        // {
+        //     Debug.Log("Error: " + error.ErrorMessage);
+        // });
+    }
+
+    private void Login()
+    {
+       
+        PlayFabClientAPI.LoginWithPlayFab(new LoginWithPlayFabRequest()
+        {
+            Username = _username.text,
+            Password = _password.text,
+        }, 
+            ResultCallback, 
+            error =>
+        {
+            Debug.Log(error.ErrorMessage);
+        });
+
+        
+
+    }
+
+    private LinkCustomIDRequest LinkCustomIDRequest()
+    {
+        var request = new LinkCustomIDRequest();
+        
+        var hasKey = PlayerPrefs.HasKey(AUTH_KEY);
+        if (!hasKey)
+            PlayerPrefs.SetString(AUTH_KEY, Guid.NewGuid().ToString());
+        
+        request.CustomId = PlayerPrefs.GetString(AUTH_KEY, Guid.NewGuid().ToString());
+        return request;
+    }
+    
+    private void Connect()
+    {
+    }
+}
